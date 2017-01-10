782ae6e7ca7b462e64895380dee85adecdd74fe5
ptr-yudai <ptr.yudai@gmail.com>
2017-01-10 16:34:36 +0900

    Unix用にインクルードファイルを変更できるように修正

--- src/tinyhsp.c
+++ src/tinyhsp.c
@@ -1,9 +1,15 @@
+#define __APPLE__
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdint.h>
 #include <GLFW/glfw3.h>
+#ifdef __APPLE__
 #include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
 #include "token.h"
 #include "lexer.h"
 #include "parser.h"
